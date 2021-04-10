3f9eca8f50bebd600c1681cc4d023526b2cc4ad8
zizkincido <49366820+zizkincido@users.noreply.github.com>
2021-04-10 10:59:56 +0300

    i changed my index file to html form

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<body> 
+<form method="POST" action="formsubmit.php">
+<p>Licence Plate:</p>
+<p><input type="text" name="licence_plate"/>
+</p>
+<p>Brand:</p>
+<p><input type="text" name="brand"/>
+</p>
+<p>Model:</p>
+<p><input type="text" name="model"/>
+</p>
+<p>Year</p>
+<p><input type="number" name="model_year"/>
+</p>
+<p>Color:</p>
+<p><input type="text" name="color"/>
+</p>
+<input type="submit" />
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
