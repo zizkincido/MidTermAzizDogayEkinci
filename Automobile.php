96fd82646360180a44f575f3a2925b1cab3c487d
zizkincido <49366820+zizkincido@users.noreply.github.com>
2021-04-10 10:43:21 +0300

    i created automobile.php and automobile class withnecessary variables and functions

---
+++ Automobile.php
@@ -0,0 +1,33 @@
+<?php
+class Automobile 
+{
+    public $licence_plate;
+    public $brand;
+    public $model;
+    public $model_year;
+    public $color;
+
+    function __construct($license_plate,$brand,$model,$model_year,$color){
+        $this->license_plate = $license_plate
+        $this->brand = $brand
+        $this->model = $model
+        $this->model_year = $model_year
+        $this->color = $color
+    }
+    function __toString(){
+        return $this->license_plate." ".$this->brand.":". $this->model.":".$this->model_year.":".$this->color
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
